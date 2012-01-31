a4c7bcd2a527fd8396ed7d0f722f5d1c07f86927
Zernyu Chou <swiftdemise@gmail.com>
2012-01-31 02:13:17 -0600

    Simple implementation of the SessionContainer: - Uses a BorderContainer layout in a template - Draggable! - Will display open tabs on page load

--- app/js/tabshare/main.js
+++ app/js/tabshare/main.js
@@ -2,11 +2,5 @@ define([
     'dojo/_base/lang',
     'tabshare/ui/SessionContainer'
 ], function(lang) {
-    chrome.tabs.query({}, function(tabs) {
-        dojo.forEach(tabs, function(tab) {
-            dojo.create("p", {innerHTML: tab.title}, dojo.body());
-        });
-    });
-
     return lang.getObject('tabshare');
 });
--- app/js/tabshare/ui/SessionContainer.js
+++ app/js/tabshare/ui/SessionContainer.js
@@ -1,10 +1,29 @@
 define([
     'module',
+    'dojo/_base/array',
     'dojo/_base/declare',
+    'dojo/_base/html',
+    'dojo/_base/lang',
+    'dojo/dnd/Moveable',
     'dojo/text!./templates/SessionContainer.html',
-    'dijit/Dialog'
-], function(module, declare, template, Dialog) {
-    return declare(module.id.replace(/\//g, '.'), [Dialog], {
-        content: 'herp'
+    'dijit/_Widget',
+    'dijit/_TemplatedMixin',
+    'dijit/_WidgetsInTemplateMixin',
+        'dijit/layout/BorderContainer',
+        'dijit/layout/ContentPane'
+], function(module, array, declare, html, lang, Moveable, template, _Widget, _TemplatedMixin, _WidgetsInTemplateMixin) {
+    return declare(module.id.replace(/\//g, '.'), [_Widget, _TemplatedMixin, _WidgetsInTemplateMixin], {
+        templateString: template,
+        baseClass: 'sessionContainer',
+
+        postCreate: function() {
+            chrome.tabs.query({}, lang.hitch(this, function(tabs) {
+                array.forEach(tabs, function(tab) {
+                    html.create("p", {innerHTML: tab.title}, this.contentBox.domNode);
+                }, this);
+            }));
+
+            var dnd = Moveable(this.domNode);
+        }
     });
 });
