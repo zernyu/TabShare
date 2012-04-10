7cdbf3e6b9a517df1cabc9d2b6020ad11dfc4445
Zernyu Chou <swiftdemise@gmail.com>
2012-04-10 00:40:51 -0500

    - Removed external libraries from source control - Moved to local copy of dojo instead of using Google CDN

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ TabShare `real name TBD!`
 ========
 
 This is a Google Chrome extension that will let users easily share tabs with themselves and each other!
+
+Dependencies
+------------
+`dojo`
+`dijit`
+`dgrid`
+`xstyle`
+`put-selector`
+
+Place these libraries in app/js
--- app/js/dojoConfig.js
+++ app/js/dojoConfig.js
@@ -3,23 +3,10 @@ window.dojoConfig = {
     async: true,
     parseOnLoad: true,
     isDebug: true,
-    baseUrl: '/app/js/',
     packages: [
         {
-            name: 'dojo',
-            location: 'http://ajax.googleapis.com/ajax/libs/dojo/1.7.2/dojo'
-        },
-        {
-            name: 'dijit',
-            location: 'http://ajax.googleapis.com/ajax/libs/dojo/1.7.2/dijit'
-        },
-        {
-            name: 'dojox',
-            location: 'http://ajax.googleapis.com/ajax/libs/dojo/1.7.2/dojox'
-        },
-        {
             name: 'tabshare',
-            location: 'tabshare'
+            location: '/app/js/tabshare'
         }
     ]
 };
